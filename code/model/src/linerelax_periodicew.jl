338cb993b5f66622a139de17a87aad6626a50d12
Noe Bigorre <bigorrenoe@gmail.com>
2023-11-30 19:51:57 +0100

    added linerelax

---
+++ code/model/src/linerelax_periodicew.jl
@@ -0,0 +1,71 @@
+function linerelax_segment(chunk, nxm, nym, nzm, cp, p, fn)
+  local info = Ref(0)
+  local rh = zeros(rc_kind, NK)
+  local subd = zeros(rc_kind, NK)
+  local supd = zeros(rc_kind, NK)
+  local dia = zeros(rc_kind, NK)
+  for j in chunk
+    local istart = div(NI, 2)
+    for ii in 1:nxm
+      local i = ii + istart
+      if (i > nxm)
+        i -= nxm
+      end
+      local im1 = i - 1
+      local ip1 = i + 1
+      if (i == 1)
+        im1 = nxm
+      end
+      if (i == nxm)
+        ip1 = 1
+      end
+      for k in 1:nzm
+        rh[k] = -(cp[2, i, j, k] * p[1+ip1, j+1, k+1]
+                  + cp[3, i, j, k] * p[1+i, j+2, k+1]
+                  + cp[4, i, j, k] * p[1+im1, j+1, k+1]
+                  + cp[5, i, j, k] * p[1+i, j, k+1]
+                  + cp[8, i, j, k] * p[1+im1, j+2, k+1]
+                  + cp[9, i, j, k] * p[1+im1, j, k+1]
+                  + cp[10, i, j, k] * p[1+ip1, j, k+1]
+                  + cp[11, i, j, k] * p[1+ip1, j+2, k+1]
+                  + cp[12, i, j, k] * p[1+im1, j+1, k]
+                  + cp[13, i, j, k] * p[1+ip1, j+1, k]
+                  + cp[14, i, j, k] * p[1+ip1, j+1, k+2]
+                  + cp[15, i, j, k] * p[1+im1, j+1, k+2]
+                  + cp[16, i, j, k] * p[1+i, j, k]
+                  + cp[17, i, j, k] * p[1+i, j+2, k]
+                  + cp[18, i, j, k] * p[1+i, j+2, k+2]
+                  + cp[19, i, j, k] * p[1+i, j, k+2]
+                  - fn[i, j, k])
+        if (k != 1)
+          subd[k] = cp[7, i, j, k]
+        end
+        if (k != nzm)
+          supd[k] = cp[6, i, j, k]
+        end
+        dia[k] = cp[1, i, j, k]
+      end
+      rh[1] -= cp[7, i, j, 1] * p[i+1, j+1, 1]
+      rh[nzm] -= cp[6, i, j, nzm] * p[i+1, j+1, nzm+2]
+
+      @ccall "./PSOM_LIB.so".dgtsl_(Ref(nzm)::Ref{Int}, pointer(subd)::Ptr{rc_kind}, pointer(dia)::Ptr{rc_kind}, pointer(supd)::Ptr{rc_kind}, pointer(rh)::Ptr{rc_kind}, info::Ref{Int})::Cvoid
+      if (info[] != 0)
+        println("error in linerelax")
+        exit(1)
+      end
+      @inbounds p[i+1, j+1, 2:nzm+1] .= rh[1:nzm]
+    end
+  end
+end
+
+
+function linerelax(nxm, nym, nzm, cp, p, fn)
+  
+  #local chunks = Iterators.partition(1:nym, div(nym, Threads.nthreads()))
+  #local tasks = map(chunks) do chunk
+  #  Threads.@spawn linerelax_segment(chunk, nxm, nym, nzm, cp, p, fn)
+  #end
+  #wait.(tasks)
+  linerelax_segment(1:nym, nxm, nym, nzm, cp, p, fn)
+
+end
--- code/model/src/mgrid.jl
+++ code/model/src/mgrid.jl
@@ -1,4 +1,4 @@
-function mgrid(p,dtime,edt,cfcdiv)
+function mgrid(p, dtime, edt, cfcdiv)
      local nx = zeros(Int, ngrid)
      local ny = zeros(Int, ngrid)
      local nz = zeros(Int, ngrid)
@@ -8,7 +8,7 @@ function mgrid(p,dtime,edt,cfcdiv)
      local loci = zeros(Int, ngrid)
      local loccp = zeros(Int, ngrid)
 
-     local cp = zeros(rc_kind, 19*maxint)
+     local cp = zeros(rc_kind, 19 * maxint)
      local rhs = zeros(rc_kind, maxint)
      local res = zeros(rc_kind, int1)
 
@@ -34,7 +34,7 @@ function mgrid(p,dtime,edt,cfcdiv)
 
      tol = edt * tol
      ntint[1] = nx[1] * ny[1] * nz[1]
-     ntout[1] = (nx[1]+2) * (ny[1]+2) * (nz[1]+2)
+     ntout[1] = (nx[1] + 2) * (ny[1] + 2) * (nz[1] + 2)
 
      loco[1] = 1
      loci[1] = 1
@@ -44,18 +44,18 @@ function mgrid(p,dtime,edt,cfcdiv)
           ny[m] = div(ny[m-1], 2)
           nz[m] = div(nz[m-1], 2)
           ntint[m] = nx[m] * ny[m] * nz[m]
-          ntout[m] = (nx[m]+2) * (ny[m]+2) * (nz[m]+2)
-          loco[m] = loco[m-1] + ntout[m - 1]
-          loci[m] = loci[m-1] + ntint[m - 1]
-          loccp[m] = loccp[m-1] + 19 * ntint[m - 1]
+          ntout[m] = (nx[m] + 2) * (ny[m] + 2) * (nz[m] + 2)
+          loco[m] = loco[m-1] + ntout[m-1]
+          loci[m] = loci[m-1] + ntint[m-1]
+          loccp[m] = loccp[m-1] + 19 * ntint[m-1]
      end
 
      cpfine(dtime, cp, rhs)
      #@ccall "./PSOM_LIB.so".cpfine_(Ref(dtime)::Ref{rc_kind}, pointer(cp)::Ptr{rc_kind}, pointer(rhs)::Ptr{rc_kind})::Cvoid
-     
+
      for m in 2:ngrid
-          local cpf = reshape(view(cp, loccp[m-1]:(loccp[m-1]- 1 +(19*nx[m] * 2 * ny[m] * 2 * nz[m] * 2))), 19, nx[m] * 2, ny[m] * 2, nz[m] * 2)
-          local cpc = reshape(view(cp, loccp[m]:(loccp[m]-1+(19*nx[m] * ny[m] * nz[m]))), 19, nx[m], ny[m], nz[m])
+          local cpf = reshape(view(cp, loccp[m-1]:(loccp[m-1]-1+(19*nx[m]*2*ny[m]*2*nz[m]*2))), 19, nx[m] * 2, ny[m] * 2, nz[m] * 2)
+          local cpc = reshape(view(cp, loccp[m]:(loccp[m]-1+(19*nx[m]*ny[m]*nz[m]))), 19, nx[m], ny[m], nz[m])
           cpcors(nx[m], ny[m], nz[m], cpf, cpc)
           #@ccall "./PSOM_LIB.so".cpcors_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp,loccp[m-1])::Ptr{rc_kind}, pointer(cp,loccp[m])::Ptr{rc_kind})::Cvoid
      end
@@ -67,36 +67,41 @@ function mgrid(p,dtime,edt,cfcdiv)
           local m = 1
 
           for l in 1:nu1
-               @ccall "./PSOM_LIB.so".linerelax_(Ref(nx[m])::Ref{Int}, pointer(ny,m)::Ptr{Int}, pointer(nz,m)::Ptr{Int}, pointer(cp,loccp[m])::Ptr{rc_kind}, pointer(p,loco[m])::Ptr{rc_kind}, pointer(rhs,loci[m])::Ptr{rc_kind})::Cvoid
+               local cp_r = reshape(view(cp, loccp[m]:(loccp[m]-1+(19*nx[m]*ny[m]*nz[m]))), 19, nx[m], ny[m], nz[m])
+               local p_r = reshape(view(p, loco[m]:(loco[m]-1+((nx[m]+2)*(ny[m]+2)*(nz[m]+2)))), (nx[m] + 2), (ny[m] + 2), (nz[m] + 2))
+               local rhs_r = reshape(view(rhs, loci[m]:(loci[m]-1+(nx[m]*ny[m]*nz[m]))), nx[m], ny[m], nz[m])
+               linerelax(nx[m], ny[m], nz[m], cp_r, p_r, rhs_r)
+               #@ccall "./PSOM_LIB.so".linerelax_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp_r)::Ptr{rc_kind}, pointer(p, loco[m])::Ptr{rc_kind}, pointer(rhs, loci[m])::Ptr{rc_kind})::Cvoid
+
                @ccall "./PSOM_LIB.so".mgpfill_(Ref(dtime)::Ref{rc_kind}, pointer(p)::Ptr{rc_kind})::Cvoid
           end
-          @ccall "./PSOM_LIB.so".resid_(Ref(m)::Ref{Int}, Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp,loccp[m])::Ptr{rc_kind}, pointer(p,loco[m])::Ptr{rc_kind}, pointer(rhs,loci[m])::Ptr{rc_kind}, pointer(res)::Ptr{rc_kind}, maxres::Ref{rc_kind})::Cvoid
+          @ccall "./PSOM_LIB.so".resid_(Ref(m)::Ref{Int}, Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp, loccp[m])::Ptr{rc_kind}, pointer(p, loco[m])::Ptr{rc_kind}, pointer(rhs, loci[m])::Ptr{rc_kind}, pointer(res)::Ptr{rc_kind}, maxres::Ref{rc_kind})::Cvoid
           if (maxres[] < tol)
                return
           end
-          @ccall "./PSOM_LIB.so".restrict_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(res)::Ptr{rc_kind}, pointer(rhs,loci[m+1])::Ptr{rc_kind})::Cvoid
+          @ccall "./PSOM_LIB.so".restrict_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(res)::Ptr{rc_kind}, pointer(rhs, loci[m+1])::Ptr{rc_kind})::Cvoid
 
           for m in ngrid:-1:2
                if m == ngrid
-                    @ccall "./PSOM_LIB.so".sor_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp,loccp[m])::Ptr{rc_kind}, pointer(p,loco[m])::Ptr{rc_kind}, pointer(rhs,loci[m])::Ptr{rc_kind})::Cvoid
+                    @ccall "./PSOM_LIB.so".sor_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp, loccp[m])::Ptr{rc_kind}, pointer(p, loco[m])::Ptr{rc_kind}, pointer(rhs, loci[m])::Ptr{rc_kind})::Cvoid
                else
                     for l in 1:nu2
-                         @ccall "./PSOM_LIB.so".linerelax_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp,loccp[m])::Ptr{rc_kind}, pointer(p,loco[m])::Ptr{rc_kind}, pointer(rhs,loci[m])::Ptr{rc_kind})::Cvoid
+                         @ccall "./PSOM_LIB.so".linerelax_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp, loccp[m])::Ptr{rc_kind}, pointer(p, loco[m])::Ptr{rc_kind}, pointer(rhs, loci[m])::Ptr{rc_kind})::Cvoid
                     end
                end
-               @ccall "./PSOM_LIB.so".efill_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(p,loco[m])::Ptr{rc_kind})::Cvoid
-               @ccall "./PSOM_LIB.so".prolong_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(p,loco[m])::Ptr{rc_kind}, pointer(p,loco[m-1])::Ptr{rc_kind})::Cvoid
+               @ccall "./PSOM_LIB.so".efill_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(p, loco[m])::Ptr{rc_kind})::Cvoid
+               @ccall "./PSOM_LIB.so".prolong_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(p, loco[m])::Ptr{rc_kind}, pointer(p, loco[m-1])::Ptr{rc_kind})::Cvoid
           end
      end
 
      local m = 1
      for l in 1:nu1
-          @ccall "./PSOM_LIB.so".linerelax_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp,loccp[m])::Ptr{rc_kind}, pointer(p,loco[m])::Ptr{rc_kind}, pointer(rhs,loci[m])::Ptr{rc_kind})::Cvoid
+          @ccall "./PSOM_LIB.so".linerelax_(Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp, loccp[m])::Ptr{rc_kind}, pointer(p, loco[m])::Ptr{rc_kind}, pointer(rhs, loci[m])::Ptr{rc_kind})::Cvoid
           @ccall "./PSOM_LIB.so".mgpfill_(Ref(dtime)::Ref{rc_kind}, pointer(p)::Ptr{rc_kind})::Cvoid
      end
 
      local m = 1
-     @ccall "./PSOM_LIB.so".resid_(Ref(m)::Ref{Int}, Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp,loccp[m])::Ptr{rc_kind}, pointer(p,loco[m])::Ptr{rc_kind}, pointer(rhs,loci[m])::Ptr{rc_kind}, pointer(res)::Ptr{rc_kind}, maxres::Ref{rc_kind})::Cvoid
+     @ccall "./PSOM_LIB.so".resid_(Ref(m)::Ref{Int}, Ref(nx[m])::Ref{Int}, Ref(ny[m])::Ref{Int}, Ref(nz[m])::Ref{Int}, pointer(cp, loccp[m])::Ptr{rc_kind}, pointer(p, loco[m])::Ptr{rc_kind}, pointer(rhs, loci[m])::Ptr{rc_kind}, pointer(res)::Ptr{rc_kind}, maxres::Ref{rc_kind})::Cvoid
 
      maxres[] = maxres[] / edt
 
