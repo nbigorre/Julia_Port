5ef5406e02ba5aaea7bc4bd6ba9bb6fd235a1031
Noe Bigorre <bigorrenoe@gmail.com>
2023-12-29 22:18:51 +0100

    added coriolis

--- code/model/src/coriolis.jl
+++ code/model/src/coriolis.jl
@@ -9,9 +9,9 @@ function coriolis(n)
   for k in 1:NK
     for j in 1:NJ
       for i in 1:NI
-        si[i, j, k] = -ffc[i, j] * v[i, j, k, n] + fac * bbc[i, j] * w[i, j, k, n] + drpx[i,j,k] - EPS * uvis[i,j,k]
-        sj[i, j, k] =  ffc[i, j] * u[i, j, k, n]                                               + drpy[i,j,k] - EPS * vvis[i,j,k]
-        sk[i, j, k] = @fortGet("fnhhy", rc_kind) * (-bbc[i, j] * u[i, j, k, n] - fac2 * (u[i, j, k, n] * u[i, j, k, n] + v[i, j, k, n] * v[i, j, k, n]) * ainv) - betainv * wvis[i,j,k]
+        si[i, j, k] = -ffc[i, j] * v[i, j, k, n] + fac * bbc[i, j] * w[i, j, k, n] + drpx[i, j, k] - EPS * uvis[i, j, k]
+        sj[i, j, k] = ffc[i, j] * u[i, j, k, n] + drpy[i, j, k] - EPS * vvis[i, j, k]
+        sk[i, j, k] = @fortGet("fnhhy", rc_kind) * (-bbc[i, j] * u[i, j, k, n] - fac2 * (u[i, j, k, n] * u[i, j, k, n] + v[i, j, k, n] * v[i, j, k, n]) * ainv) - betainv * wvis[i, j, k]
       end
     end
   end
